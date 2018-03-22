69ec996505d36fc9f2cd56a1b1f66a3b5d4796b2
Jairo Medrano <jairouriel1998@gmail.com>
2018-03-22 14:20:53 -0600

    creando nuevas tablas

--- Krankenhaus_Database.sql
+++ Krankenhaus_Database.sql
@@ -3,5 +3,16 @@ CREATE DATABASE Krankenhaus_Database;
 USE Krankenhaus_Database;
 
 CREATE TABLE Hospital(
-  codigo VARCHAR(20) PRIMARY KEY
+  C_hospital VARCHAR(20) PRIMARY KEY
+);
+
+CREATE TABLE Personas(
+  C_persona VARCHAR(30) PRIMARY KEY,
+  C_hospital VARCHAR(30),
+  Nombres VARCHAR(30),
+  Apellidos VARCHAR(30),
+  Edad INT,
+  Sexo VARCHAR(10),
+  Telefono VARCHAR(8),
+  Direccion TEXT 
 );
